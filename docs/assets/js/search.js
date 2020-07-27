a822c1744f52df989edc1097fe3d2a529e4c1456
Tony Wooster <tony.wooster@contentful.com>
2020-07-27 16:33:05 +0200

    chore: update docs

--- docs/assets/js/search.js
+++
@@ -1,3 +0,0 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"65536":"Type literal"},"rows":[{"id":0,"kind":65536,"name":"__type","url":"interfaces/pooloptions.html#create.__type","classes":"tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported","parent":"PoolOptions.create"},{"id":1,"kind":65536,"name":"__type","url":"interfaces/releasableitem.html#release.__type","classes":"tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported","parent":"ReleasableItem.release"},{"id":2,"kind":65536,"name":"__type","url":"classes/pool.html#createcb.__type","classes":"tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported","parent":"Pool.createCb"}]};
